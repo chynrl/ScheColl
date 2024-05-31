b1f6704aa90e557a9f5fe5492b94cb037e2b1dce
nurul <chynrl18@gmail.com>
2024-05-31 20:15:00 +0700

    add

--- resources/views/deletematkul.blade.php
+++ resources/views/deletematkul.blade.php
@@ -4,7 +4,7 @@
 <h2>Apakah anda yakin akan menghapus mata kuliah {{$matkul->nama_mata_kuliah}}?</h2>
 <form action="{{route('actiondeletematkul', ['id'=>$matkul->id])}}" method="post">
     @csrf 
-    // tombol pilihan
+    {{-- tombol pilihan --}}
     <button type="submit" class="btn btn-danger btn-block"> Yes </button>
     <a class="btn btn-block" href="{{route('matkul')}}" role="botton">No</a>
         
